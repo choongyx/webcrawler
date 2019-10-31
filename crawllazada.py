5aa8e7fc7dd5204f785fc7521ce0989c247a6f89
crizyli <liyufei6666@gmail.com>
2019-10-31 21:13:06 +0800

    Lazada crawler

---
+++ crawllazada.py
@@ -0,0 +1,39 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+
+def crawl_lazada(keyword):
+
+    url = 'https://www.lazada.sg/catalog/?q='+keyword
+    f = open("items.json", "w+")
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    s = soup.find_all('script', limit=4)
+    f.write(str(s[3])[24:-9])
+    f.close()
+    text = json.load(open("items.json", "r"))
+
+    count = 0
+    itemList = []
+    for item in text['mods']['listItems']:
+        itemList.append((item['name'], float(item['price']), item['productUrl'][2:]))
+        if count == 20:
+            break
+        count += 1
+
+    sorted_itemList = sorted(itemList, key=lambda x: x[1])
+
+    return sorted_itemList[10:]
+
+if __name__ == "__main__": 
+    keyword = 'iphone'
+
+    sorted_itemList = crawl_lazada(keyword)
+
+    for item in sorted_itemList:
+        print(item[0])
+        print(item[1])
+        print(item[2])
+        print("--------------------------------------------------------------------------")
+
